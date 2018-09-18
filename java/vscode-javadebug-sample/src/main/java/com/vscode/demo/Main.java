3794cb66506cc49d2021415be84d25ead78c29f2
Carlos dos Santos <cds@cds-software.com.br>
2018-09-18 12:59:39 -0300

    Ignite Java

--- java/vscode-javadebug-sample/src/main/java/com/vscode/demo/Main.java
+++ java/vscode-javadebug-sample/src/main/java/com/vscode/demo/Main.java
@@ -8,8 +8,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello VS Code Debugging at Ignite!");
 
+        String find = "VSCode";    
+
         Stream.of("React", "Angular", "VSCode","Cool Editor")
-            .filter(x -> Objects.equals(x, "VSCode"))
+            .filter(x -> Objects.equals(x, find))
             .forEach(System.out::println);
     }
 
