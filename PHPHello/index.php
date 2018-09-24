871752bd94feb9a6d792095d3c97555ae7f19766
Carlos dos Santos <cds@cds-software.com.br>
2018-09-24 17:23:44 -0300

    Ignite

--- PHPHello/index.php
+++ PHPHello/index.php
@@ -1,12 +1,8 @@
-<?php 
-
-$s = sum(10,20);
-$date = date('m/d/Y h:i:s a', time());
-echo "\nVSCode running PHP at Ignite! - ($date)\n"; 
-
-
-function sum($a, $b): float {
-    return $a + $b;
-} 
-
-?>
+<html>
+ <head>
+  <title>Ignite</title>
+ </head>
+ <body>
+ <?php echo "<p>PHP running on Docker at Ignite!</p>"; ?>
+ </body>
+</html>
